6eb613268fc95e5d4f1fd6f8f4e16922f4acf084
Norbert Benczúr <nukesz@users.noreply.github.com>
2025-08-24 20:17:12 +0000

    Adds simple webpage to display the news

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import axios from "axios"
+
+const news = ref([])
+
+onMounted(async () => {
+  const apiUrl = import.meta.env.VITE_API_URL
+  const res = await axios.get(apiUrl)
+  news.value = res.data
+})
+</script>
+
+<template>
+   <div class="p-4">
+    <h1>Top 10 News</h1>
+    <ul>
+      <li v-for="item in news" :key="item.id">
+        <strong>{{ item.title }}</strong> - {{ item.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
